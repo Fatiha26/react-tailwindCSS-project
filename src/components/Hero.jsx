a53026d0d276ecc7367d6d685d83ba9d01270bce
Lamia Fatiha <f6lamia@gmail.com>
2024-06-27 12:16:08 +0600

    hero section done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,14 +4,14 @@ import video2 from "../assets/video2.mp4";
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center mt-6 lg:mt-10'>
+    <div className='flex flex-col items-center mt-5 lg:mt-5'>
     <h1 className='text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide'>
     VirtualR build tools 
     <span className='bg-gradient-to-r from-orange-500 to to-red-800 text-transparent bg-clip-text'>
      {" "}  for developers
     </span>
     </h1>
-    <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+    <p className='mt-6 text-lg text-center text-neutral-500 max-w-4xl'>
          Known for their SteamVR platform and the Valve Index headset, Valve is a 
          significant player in the VR gaming space!
     </p>
@@ -21,12 +21,12 @@ const Hero = () => {
     </div>
     <div className='flex mt-10 justify-center'>
      <video autoPlay loop muted
-     className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+     className='rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2 my-4'>
         <source src={video1} type='video/mp4 '/>browser does not support
      </video>
 
      <video autoPlay loop muted
-     className='rounded-lg w-1/2 border border-orange-700 shadow-orange-400 mx-2 my-4'>
+     className='rounded-lg w-1/3 border border-orange-700 shadow-orange-400 mx-2 my-4'>
         <source src={video2} type='video/mp4 '/>browser does not support
      </video>
 
