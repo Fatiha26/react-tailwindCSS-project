af7d47ba65e890814691ccce9c2a433928d1df34
Lamia Fatiha <f6lamia@gmail.com>
2024-06-27 13:25:18 +0600

    feature section done

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -6,7 +6,8 @@ const Feature = () => {
     <div className='relative mt-20 border-b border-neutral-800 min-h-{800px}'>
        <div className="text-center">
         <span className='bg-neutral-900 text-orange-500 rounded-full h-6 text-sm
-        font-medium px-2 py-1 uppercase'>Feature</span>
+        font-medium px-2 py-1 uppercase'>Feature
+        </span>
         <h2 className='text-3xl sm:text-5xl lg:text-6xl mt-5 lg:mt-10 tracking-wide'>
         Easily build
         <span className='bg-gradient-to-r from-orange-500 to to-red-800 text-transparent bg-clip-text'>
@@ -14,7 +15,7 @@ const Feature = () => {
         </span>
         </h2>
        </div>
-    <div className='flex flex-wrap mt-5 lg:mt-10'>
+    <div className='flex flex-wrap mt-5 lg:mt-20 mx-10'>
     {
         features.map((feature, index)=>(
             <div key={index} className='w-full sm:1/2 lg:w-1/3'>
@@ -22,18 +23,18 @@ const Feature = () => {
                 <div className='flex bg-neutral-900 text-orange-700 h-10 w-10 mx-10
                   p-2 justify-between items-center rounded-full'>
                     {feature.icon}
-
-                </div>
-
               </div>
-
+           <div>
+              <h5 className='mt-1 mb-2 text-md'>{feature.text}</h5>
+              <p className='p-2 mb-10 text-sm text-neutral-500 text-justify'>{feature.description}
+              </p>
             </div>
-        ))
-    }
-
+            </div>
+            </div>
+        ))}
     </div>
     </div>
-  )
-}
+  );
+};
 
-export default Feature
+export default Feature;
