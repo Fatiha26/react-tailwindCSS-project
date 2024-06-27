1673f1616f2763e2744885356b4256299954542a
Lamia Fatiha <f6lamia@gmail.com>
2024-06-27 13:39:11 +0600

    workflow heading done

--- src/components/Workflow.jsx
+++ src/components/Workflow.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { CheckCircle2 } from 'lucide-react';
+import codeImg from "../assets/code.jpg";
 
 const Workflow = () => {
   return (
-    <div>
+    <div className='mt-20'>
+        <h2 className='text-3xl sm:text-5xl lg:text-5xl mt-6 lg:mt-10 tracking-wide text-center'>
+           Accelerate your
+        <span className='bg-gradient-to-r from-orange-500 to to-red-800 text-transparent bg-clip-text'>
+        {" "}your coding workflow.
+        </span>
+        </h2>
     </div>
   )
 }
