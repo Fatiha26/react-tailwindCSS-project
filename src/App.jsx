f31c9241836ea0e746be0a678efc4a743db8249e
Lamia Fatiha <f6lamia@gmail.com>
2024-06-27 12:18:22 +0600

    starting feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import React from "react";
+import Feature from "./components/Feature";
 const App = () => {
   return (
     <>
@@ -8,6 +9,7 @@ const App = () => {
     <div className="max-w-7xl mx-auto pt-20 px-6">
        <Hero />
     </div>
+    <Feature />
     </>
   );
 };
