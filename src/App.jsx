92c050ef81d54e6edc199e1938a09c2589f8cfe1
Lamia Fatiha <f6lamia@gmail.com>
2024-06-28 12:29:46 +0600

    testimonial section done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Feature from "./components/Feature";
 import Workflow from "./components/Workflow";
 import Pricing from "./components/Pricing";
+import Testimonial from "./components/Testimonial";
 const App = () => {
   return (
     <>
@@ -14,6 +15,7 @@ const App = () => {
     <Feature />
     <Workflow />
     <Pricing />
+    <Testimonial />
     </>
   );
 };
